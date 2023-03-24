ba689646d640d3c136bc57f2077af84aa71a32ff
Daniel Schreurs <daniel.schreurs@hotmail.com>
2023-03-24 08:02:07 +0100

    Introduce circle movement

---
+++ src/ts/shapes/Circle.ts
@@ -0,0 +1,54 @@
+import {Hsl} from "../Colors/Hsl";
+import {Rgb} from "../Colors/Rgb";
+import {Position} from "../Types/position";
+import {Canvas} from "../Canvas";
+import {Shape} from "./Shape";
+import {Animatable} from "../Types/Animatable";
+
+export class Circle extends Shape implements Animatable {
+    protected readonly radius: number;
+
+    constructor(canvas: Canvas, color: Hsl | Rgb, radius: number, position: Position, speed?: number, direction?: number) {
+        super(canvas, position, speed, direction, color);
+        this.radius = radius;
+    }
+
+    draw() {
+        this.ctx.beginPath();
+        this.ctx.fillStyle = `${this.color}`;
+        this.ctx.arc(this.position.x,this.position.y,this.radius,0,Math.PI*2);
+        this.ctx.fill();
+        this.ctx.closePath();
+        return this;
+    }
+
+    clear() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    update() {
+        // Calculer la prochaine position du cercle en fonction de sa vitesse et de sa direction
+        const nextX = this.position.x + this.speed * Math.cos(this.direction);
+        const nextY = this.position.y + this.speed * Math.sin(this.direction);
+
+        // Vérifier si le cercle touche le bord droit ou gauche de la zone de dessin
+        if (nextX + this.radius >= this.canvas.width || nextX - this.radius <= 0) {
+            // Inverser la direction horizontale du cercle
+            this.direction = Math.PI - this.direction;
+        }
+
+        // Vérifier si le cercle touche le bord haut ou bas de la zone de dessin
+        if (nextY + this.radius >= this.canvas.height || nextY - this.radius <= 0) {
+            // Inverser la direction verticale du cercle
+            this.direction = -this.direction;
+        }
+
+        // Mettre à jour la position du cercle
+        this.position.x += this.speed * Math.cos(this.direction);
+        this.position.y += this.speed * Math.sin(this.direction);
+    }
+
+    setDirectionByMousePosition(position: Position) {
+        this.direction = Math.atan2(position.y - this.position.y, position.x - this.position.x);
+    }
+}
