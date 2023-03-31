286f6a36cc096a3e41a35d740bddcb14c318b3a6
Daniel Schreurs <daniel.schreurs@hotmail.com>
2023-03-31 09:39:57 +0200

    :tada: Initial commit

---
+++ src/ts/Colors/Hsla.ts
@@ -0,0 +1,10 @@
+import {Hsl} from "./Hsl";
+
+export class Hsla extends Hsl {
+    private readonly alpha: number;
+
+    constructor(hue: number, saturation: number, lightness: number, alpha: number) {
+        super(hue, saturation, lightness);
+        this.alpha = alpha;
+    }
+}
---
+++ src/ts/Colors/Rgba.ts
@@ -0,0 +1,10 @@
+import {Rgb} from "./Rgb";
+
+export class Rgba extends Rgb {
+    private readonly alpha: number;
+
+    constructor(red: number, green: number, blue: number, alpha: number) {
+        super(red, green, blue);
+        this.alpha = alpha;
+    }
+}
