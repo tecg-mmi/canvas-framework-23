1d40792b5a56640d5f41bca04f99259399351cc2
sacha-meunier <meuniersacha@gmail.com>
2023-05-12 10:55:38 +0200

    adding documentation for square class

--- src/ts/Shapes/Square.ts
+++ src/ts/Shapes/Square.ts
@@ -1,24 +1,46 @@
+/*
+* The class Square extends the Shape class and implements the Animatable interface.
+* This class allows you to draw, clear and update a square shape on the canvas.
+* */
 import {Shape} from "./Shape";
 import {ISquare} from "../Types/ISquare";
 import {Animatable} from "../Types/Animatable";
 
-export class Square extends Shape implements Animatable{
+export class Square extends Shape implements Animatable {
+    /*
+    * Side represents the length of a side of the square.
+    * */
     protected readonly side: number;
 
-    constructor(square:ISquare) {
+    /*
+    * Creates a new square object
+    * @param square - An object containing information about the square
+    * */
+    constructor(square: ISquare) {
         super(square);
         this.side = square.side;
     }
 
+    /*
+    * Draws a filled square on the canvas using the : x and y of it's starting positions and it's
+    * width and height.
+    * */
     draw(): void {
         this.ctx.fillStyle = `${this.color}`;
         this.ctx.fillRect(this.position.x, this.position.y, this.side, this.side);
     }
 
+    /*
+    * Clears the square from the canvas.
+    * */
     clear(): void {
         this.ctx.clearRect(this.position.x, this.position.y, this.side, this.side);
     }
 
+    /**
+     * Updates the square. (Not implemented)
+     * @throws Error - This method is not implemented.
+     */
     update(): void {
         throw new Error("Method not implemented.");
     }
