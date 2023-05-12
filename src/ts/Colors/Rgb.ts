0ed4ffb27f01e398ef07a2ee1fdc56988ece89c8
kael-cadranel <kael.cadranel00@gmail.com>
2023-05-12 10:19:46 +0200

    updated class Rgb and Rgba

--- src/ts/Colors/Rgb.ts
+++ src/ts/Colors/Rgb.ts
@@ -1,3 +1,11 @@
+/**
+* An RGB color
+*
+* @param red - The red component of the RGB color int(0-255).
+* @param green - The green component of the RGB color int(0-255).
+* @param blue - The blue component of the RGB color int(0-255).
+*/
+
 export class Rgb {
     protected readonly red: number;
     protected readonly green: number;
@@ -9,6 +17,9 @@ export class Rgb {
         this.blue = blue;
     }
 
+    /**
+     * @return a string of the color in rgb form (rgb(r,g,b))
+     */
     public toString = (): string => {
         return `rgb(${this.red},${this.green},${this.blue})`;
     }
--- src/ts/Colors/Rgba.ts
+++ src/ts/Colors/Rgba.ts
@@ -1,5 +1,14 @@
 import {Rgb} from "./Rgb";
 
+/**
+ * An RGBa color
+ *
+ * @extends Rgb
+ * @param red - The red component of the RGB color int(0-255).
+ * @param green - The green component of the RGB color int(0-255).
+ * @param blue - The blue component of the RGB color int(0-255).
+ * @param alpha - The alpha component of the RGB color float(0-1).
+ */
 export class Rgba extends Rgb {
     private readonly alpha: number;
 
@@ -8,7 +17,9 @@ export class Rgba extends Rgb {
         this.alpha = alpha;
     }
 
-
+    /**
+     * @return a string of the color in rgba form (rgb(r,g,b,a))
+     */
     public toString = (): string => {
         return `rgba(${this.red},${this.green},${this.blue},${this.alpha})`;
     }
