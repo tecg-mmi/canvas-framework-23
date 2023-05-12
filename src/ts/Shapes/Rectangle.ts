999e97e7ada85db8d9d79f9ba313c893aaf15d94
NathanFortin <fortinnathan87@gmail.com>
2023-05-12 11:19:42 +0200

    :memo: add docs for Rectangle

--- src/ts/Shapes/Rectangle.ts
+++ src/ts/Shapes/Rectangle.ts
@@ -3,27 +3,48 @@ import {Shape} from "./Shape";
 import {Animatable} from "../Types/Animatable";
 import {IRectangle} from "../Types/IRectangle";
 
+/**
+ * Represents a rectangle shape on the canvas that can be animated.
+ * 
+ * @exports Rectangle
+ * @extends Shape
+ * @implements Animatable
+ */
 export class Rectangle extends Shape implements Animatable {
+
     protected readonly width: number;
     protected readonly height: number;
 
+     /**
+     * Constructs a new Rectangle object.
+     *
+     * @param rectangle An object containing the initial properties of rectangle (width, heigth) extended of shape (canvas, position, color, speed[optional], direction[optional]
+     * Must have a 'width' and 'height' property of type number.
+     * @interface IRectangle
+     */
     constructor(rectangle: IRectangle) {
         super(rectangle);
         this.width = rectangle.width;
         this.height = rectangle.height;
     }
 
-
+    /**
+     * Draws the rectangle on the canvas
+     * @returns The current Rectangle object.
+     */
     draw() {
-        this.ctx.save(); // sauvegarde l'état actuel du contexte
-        this.ctx.translate(Math.trunc(this.position.x + this.width / 2), Math.trunc(this.position.y + this.height / 2)); // déplace l'origine du système de coordonnées au centre du rectangle
-        this.ctx.rotate(this.direction); // applique la transformation de rotation
+        this.ctx.save(); // save the current state of the context
+        this.ctx.translate(Math.trunc(this.position.x + this.width / 2), Math.trunc(this.position.y + this.height / 2)); // move the origin of the coordinate system to the center of the rectangle
+        this.ctx.rotate(this.direction); // apply the rotation transformation
         this.ctx.fillStyle = `${this.color}`;
-        this.ctx.fillRect(-Math.trunc(this.width / 2), -Math.trunc(this.height / 2), this.width, this.height); // dessine le rectangle centré sur l'origine
-        this.ctx.restore(); // restaure l'état précédent du contexte
+        this.ctx.fillRect(-Math.trunc(this.width / 2), -Math.trunc(this.height / 2), this.width, this.height); // draw the rectangle centered at the origin
+        this.ctx.restore(); // restore the previous state of the context
         return this;
     }
 
+    /**
+     * Updates the position and direction of the rectangle based on its speed and current direction.
+     */
     update() {
         this.position.x += Math.floor(this.speed * Math.cos(this.direction));
         this.position.y += Math.floor(this.speed * Math.sin(this.direction));
@@ -37,12 +58,20 @@ export class Rectangle extends Shape implements Animatable {
         }
     }
 
+ /**
+     * Sets the direction of the rectangle based on the position of the mouse cursor.
+     *
+     * @param position The current position of the mouse cursor.
+     */
     setDirectionByMousePosition(position: Position) {
         this.direction = Math.atan2(position.y - this.position.y, position.x - this.position.x);
     }
 
+    /**
+     * Clears the rectangle from the canvas.
+     * @throws This method is not implemented.
+     */
     clear(): void {
         throw new Error("Method not implemented.");
     }
-
 }
