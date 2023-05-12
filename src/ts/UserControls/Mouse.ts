82253f8e78f225936823578b5f5c5f5f8ce63371
Bruno Marée <bruno.maree@student.hepl.be>
2023-05-12 11:17:31 +0200

    add commentary

--- src/ts/UserControls/Mouse.ts
+++ src/ts/UserControls/Mouse.ts
@@ -1,7 +1,15 @@
 import {Circle} from "../Shapes/Circle";
 import {IMouse} from "../Types/IMouse";
 
+/*
+* It represents a mouse cursor (extended from Circle) on a canvas and allows tracking its position using the mousemove event.
+ */
+
 export class Mouse extends Circle {
+    /*
+    * The constructor initializes the Mouse object with the provided IMouse parameter and adds a mousemove event listener to update the mouse position.
+    * The listener callback is triggered, and the x and y coordinates of the position property are updated based on the offsetX and offsetY values of the event object.
+     */
     constructor(mouse: IMouse) {
         super({
             position: {x: 0, y: 0},
@@ -14,6 +22,10 @@ export class Mouse extends Circle {
             this.position.y = event.offsetY;
         });
     }
+
+    /*
+    * The update() method is currently empty and needs to be implemented.
+     */
 
     update() {
         //
